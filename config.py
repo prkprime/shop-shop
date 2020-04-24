729d063482ca1dd4b81fa906afd43bced0567f5b
Pratik Gorade <pratikgorade0@gmail.com>
2020-04-24 20:35:07 +0530

    added remote db

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ WTF_CSRF_ENABLED = True
 SECRET_KEY = 'prkaly007'
 DB_NAME = 'primecart'
 
-DATABASE = MongoClient()[DB_NAME]
+DATABASE = MongoClient('mongodb+srv://prkprime:prkaly007@primecart-uvg6p.gcp.mongodb.net/test?retryWrites=true&w=majority')[DB_NAME]
 USERS_COLLECTION = DATABASE.users
 
 DEBUG = True
--- primecart/__init__.py
+++ primecart/__init__.py
@@ -6,12 +6,12 @@ from werkzeug.security import generate_password_hash
 from pymongo.errors import DuplicateKeyError
 from primecart.user import User
 
-
 app = Flask(__name__)
 app.config.from_object('config')
 loginmanager = LoginManager()
 loginmanager.init_app(app)
 loginmanager.login_view = 'login'
+
 
 @loginmanager.user_loader
 def load_user(username):
@@ -20,20 +20,23 @@ def load_user(username):
         return None
     return User(u['_id'], u['user_type'])
 
+
 @app.route('/')
 @login_required
 def index():
     return render_template('index.html')
+
 
 @app.route('/admin')
 @login_required
 def admin():
     return render_template('admin.html')
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        user = app.config['USERS_COLLECTION'].find_one({'_id' : request.form['username']})
+        user = app.config['USERS_COLLECTION'].find_one({'_id': request.form['username']})
         if user and User.validate_login(user['password'], request.form['password']):
             user_object = User(user['_id'], user['user_type'])
             login_user(user_object)
@@ -45,10 +48,12 @@ def login():
         print("Wrong username or password!")
     return render_template('login.html')
 
+
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('login'))
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -58,7 +63,7 @@ def register():
         user_type = request.form['user_type']
         pass_hash = generate_password_hash(password, method='pbkdf2:sha256')
         try:
-            app.config['USERS_COLLECTION'].insert_one({'_id' : username, 'password' : pass_hash, 'user_type' : user_type})
+            app.config['USERS_COLLECTION'].insert_one({'_id': username, 'password': pass_hash, 'user_type': user_type})
             print('User created successfully')
             return redirect(url_for('login'))
         except DuplicateKeyError:
