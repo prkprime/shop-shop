891d0e671e1acd1702e4434e52bfbab1c5001472
Pratik Gorade <pratikgorade0@gmail.com>
2020-04-29 13:30:19 +0530

    transactions were list of list of strings of ProductId instead of int. typecasted that to int

--- primecart/generate_association_rules.py
+++ primecart/generate_association_rules.py
@@ -1,7 +1,10 @@
 from pymongo import MongoClient
 import pyfpgrowth
+import time
 
 def get_transactions():
+    print('retriving transactions from database')
+    start_time = time.time()
     DATABASE = MongoClient()['primecart']
     purchase_data = DATABASE.PurchaseData.find()
     transaction_dict = {}
@@ -15,12 +18,27 @@ def get_transactions():
             single_transaction.append(i['ProductId'])
             transaction_dict[i['InvoiceId']] = single_transaction
     transaction = list(transaction_dict.values())
+    transaction2 = []
+    for i in transaction:
+        single_transaction = []
+        for j in i:
+            try:
+                temp = int(j)
+                single_transaction.append(temp)
+            except ValueError:
+                pass
+            except TypeError:
+                pass
+        if len(single_transaction) != 0:
+            transaction2.append(single_transaction)
+    print(transaction2[0])
+    print(type(transaction2[0][0]))
+    print(f'transactions retrived. (time taken : {round(time.time()-start_time, 4)} seconds)')
     return transaction
 
 def get_rules(transactions):
-    print('transactions retrived')
     print('generating patterns...')
-    patterns = pyfpgrowth.find_frequent_patterns(transactions, 400)
+    patterns = pyfpgrowth.find_frequent_patterns(transactions, 40)
     print('patterns generated')
     print('generating rules...')
     return pyfpgrowth.generate_association_rules(patterns, 0.7)
